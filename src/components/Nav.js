928a845150491a55c4b2c9691a236496ec494152
pratik_guha <sguha4966@gmail.com>
2024-11-19 21:04:31 +0530

    pg update this

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,44 @@
-import React from 'react'
-import '../css/nav.css'
+import React from 'react';
+import '../css/nav.css';
+import { Link } from 'react-router-dom';
+
+const menuItem = [
+  {
+    path: "/",
+    name: "Home",
+  },
+  {
+    path: "/about",
+    name: "About",
+  },
+  {
+    path: "/discover",
+    name: "Discover",
+  },
+  {
+    path: "/place",
+    name: "Place",
+  },
+];
 
 const Nav = () => {
   return (
-    <header className="navbar">
-      <h1 className="brand">Travel Planner</h1>
-      <nav className="menu">
-        <div className="nav-center">
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#discover">Discover</a>
-          <a href="#place">Place</a>
-          <a href="#registration">Registration</a>
-        </div>
-        <div className="nav-right">
-          <a href="#login">Login/Signup</a>
-        </div>
+    <header className="flex justify-between items-center px-10 py-5 bg-transparent">
+      <h1 className="md:text-2xl text-xl font-medium md:font-semibold ">Travel Planner</h1>
+      <nav className="flex gap-4">
+        {menuItem.map((item) => (
+          <Link key={item.name} to={item.path} className="text-base md:text-xl hover:underline">
+            {item.name}
+          </Link>
+        ))}
       </nav>
+      <div >
+        <Link href="#login" className="text-base md:text-xl hover:underline">
+          Login/Signup
+        </Link>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
